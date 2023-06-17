4a4087fe1bbe90954f9c047c088b18dea2ccffb2
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-17 17:26:45 -0500

    fix layout errors header add footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import CartButton from './CartButton'
 
 const Header = () => {
   return (
-    <header className='w-full  max-w flex justify-center'>
+    <header className='w-full flex justify-center'>
       <nav className='flex py-5 px-2 max-w-6xl items-center justify-between w-full'>
         <img src='/store_icon.png' alt='store icon' className='w-14 sm:w-24 xs:w-16 h-auto rounded-lg hover:scale-110' />
-        <CartButton active={true} />
+        <CartButton active={false} />
       </nav>
     </header>
   )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,16 +4,13 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>
 
-        {
-          children
-        }
+      {
+        children
+      }
 
-      </main>
-
-      <footer>
-        footer
+      <footer className='w-full text-center py-14  max-w-6xl'>
+        <h2 className='text-gray-400 font-semibold border-fuchsia-600 border-t py-4'>2023</h2>
       </footer>
     </>
   )
