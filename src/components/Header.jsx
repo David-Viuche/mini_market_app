a16c5ec32fee35ba674fb36cbb22c7f312747d8e
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-18 16:13:46 -0500

    add link on header to reset page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom'
 import CartButton from './CartButton'
+import { useUserActions } from '../hooks/useProductActions'
 
 const Header = () => {
+  const { resetSelectedProduct } = useUserActions()
+
   return (
     <header className='w-full flex justify-center'>
       <nav className='flex py-5 px-2 max-w-6xl items-center justify-between w-full'>
-        <img src='/store_icon.png' alt='store icon' className='w-14 sm:w-24 xs:w-16 h-auto rounded-lg hover:scale-110' />
+        <Link onClick={resetSelectedProduct} to='/'>
+          <img src='/store_icon.png' alt='store icon' className='w-14 sm:w-24 xs:w-16 h-auto rounded-lg hover:scale-110' />
+        </Link>
         <CartButton active={false} />
       </nav>
     </header>
