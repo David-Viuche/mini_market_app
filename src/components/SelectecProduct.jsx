246ff62c5b277693c63dff88e38960f2ea96179d
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-18 00:37:24 -0500

    add respondive desing to selected product

--- src/components/SelectecProduct.jsx
+++ src/components/SelectecProduct.jsx
@@ -1,12 +1,27 @@
 import { useSelector } from 'react-redux'
 import ButtonsAddProduct from './ButtonsAddProduct'
 import BasicDataProduct from './BasicDataProduct'
+import Close from './svg/Close'
+import { useUserActions } from '../hooks/useProductActions'
 
 const SelectecProduct = () => {
   const selectedProduct = useSelector(state => state.products.selectedProduct)
+  const { resetSelectedProduct } = useUserActions()
+
+  const handleOnclick = () => {
+    resetSelectedProduct()
+  }
+
   return (
-    <section className='sm:w-2/5'>
-      <h1 className='text-fuchsia-600 font-bold text-xl border-b mb-4 p-2 border-fuchsia-600' >Product</h1>
+    <section className={`sm:w-2/5 ${(!selectedProduct) && 'hidden'} absolute z-10 top-24 bg-white inset-x-0 sm:block  sm:relative sm:top-0`}>
+      <div className='border-b mb-4 p-2 border-fuchsia-600 flex items-center justify-between'>
+        <h1 className='text-fuchsia-600 font-bold text-xl '>Product</h1>
+        {
+          selectedProduct && <button onClick={() => (handleOnclick())} className='sm:hidden font-bold text-xl text-white hover:scale-110 bg-fuchsia-600 h-full flex items-center justify-center p-2'>
+            <Close />
+          </button>
+        }
+      </div>
       {
         !selectedProduct && <h2 className='text-gray-400 font-semibold'>Please choose a product in the left.</h2>
       }
@@ -18,7 +33,7 @@ const SelectecProduct = () => {
               <BasicDataProduct product={selectedProduct} />
               <ButtonsAddProduct />
             </div>
-            <p className='text-gray-400 font-semibold py-4 border-b border-fuchsia-600'>{selectedProduct.description}</p>
+            <p className='text-gray-400 font-semibold p-4 border-b border-fuchsia-600'>{selectedProduct.description}</p>
           </div>
         )
       }
