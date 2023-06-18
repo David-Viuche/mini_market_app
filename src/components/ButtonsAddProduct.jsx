c6ad7b92966999e623f8b28427630adfb15dcdd0
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-18 13:52:44 -0500

    fix buttons add and remove cart styles

--- src/components/ButtonsAddProduct.jsx
+++ src/components/ButtonsAddProduct.jsx
@@ -14,11 +14,11 @@ const ButtonsAddProduct = () => {
   }
 
   return (
-    <div className='flex items-center justify-center text-2xl'>
-      <button onClick={handleOnclickRemove} className='font-bold  text-black hover:scale-110 hover:bg-gray-200 bg-gray-300 h-8 w-8 flex items-center justify-center px-2'>
+    <div className='flex items-center justify-center sm:justify-end text-2xl'>
+      <button onClick={handleOnclickRemove} className='font-bold  text-black hover:scale-110 hover:bg-gray-200 bg-gray-300 h-8 w-32 sm:w-8 flex items-center justify-center px-2'>
         -
       </button>
-      <button onClick={handleOnclick} className='font-bold  text-white hover:scale-110 hover:bg-fuchsia-400 bg-fuchsia-600 h-8 w-8 flex items-center justify-center px-2'>
+      <button onClick={handleOnclick} className='font-bold  text-white hover:scale-110 hover:bg-fuchsia-400 bg-fuchsia-600 h-8 w-32 sm:w-8 flex items-center justify-center px-2'>
         +
       </button>
     </div>
