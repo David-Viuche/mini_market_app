40c9928a03b36be22822d06a7d6efc49c804e321
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-18 20:03:06 -0500

    delete footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,9 +9,6 @@ const Layout = ({ children }) => {
         children
       }
 
-      <footer className='w-full text-center py-14  max-w-6xl'>
-        <h2 className='text-gray-400 font-semibold border-fuchsia-600 border-t py-4'>2023</h2>
-      </footer>
     </>
   )
 }
