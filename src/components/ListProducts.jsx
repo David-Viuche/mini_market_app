a9431eab1b3e778db71c21b70cb48fda930d3012
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-18 20:09:30 -0500

    add two columns to mobile desing

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -25,7 +25,7 @@ const ListProducts = () => {
   return (
     <section className={`sm:w-3/5 ${(selectedProduct) && 'hidden'} ${(activeCart) && 'hidden'} sm:block w-full`}>
       <h1 className='text-fuchsia-600 font-bold text-xl border-b mb-4 p-2 border-fuchsia-600' >Store</h1>
-      <div className='columns-1 sm:columns-2 md:columns-3 gap-10 grid place-items-center sm:block'>
+      <div className='columns-2 md:columns-3 gap-10 p-2 sm:block'>
         {
           !products && 'No products'
         }
