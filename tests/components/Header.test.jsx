204e257305f493b78f4e76e6e44c29191310dafc
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-06-18 21:50:10 -0500

    change header test

--- tests/components/Header.test.jsx
+++ tests/components/Header.test.jsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Header from '../../src/components/Header'
+import { Provider } from 'react-redux'
+import { store } from '../../src/store/store'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('Header_component', () => {
   it('test_header_renders_without_errors', () => {
-    render(<Header />)
+    render(
+      <Provider store={store}>
+        <MemoryRouter basename='/'>
+          <Header />
+        </MemoryRouter>
+      </Provider >
+    )
     const headerElement = screen.getByRole('banner')
     expect(headerElement).toBeInTheDocument()
 
@@ -13,14 +22,13 @@ describe('Header_component', () => {
   })
 
   it('test_store_icon_image_is_displayed', () => {
-    render(<Header />)
+    render(
+      <Provider store={store}>
+        <MemoryRouter basename='/'>
+          <Header />
+        </MemoryRouter>
+      </Provider >
+    )
     expect(screen.getByAltText('store icon')).toBeInTheDocument()
-  })
-
-  it('test_header_renders_correctly', () => {
-    render(<Header />)
-    const images = screen.getAllByRole('img')
-
-    expect(images.length).toBe(1)
   })
 })
